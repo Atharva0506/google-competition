3d90edca5b57a4e12a0d9c10a834310418dae6e2
Atharva Naik <atharvan.coder@gmail.com>
2024-07-26 16:48:40 +0530

    footer btn done

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from "./main/main.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { FooterBtnComponent } from "./footer-btn/footer-btn.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainComponent, SidebarComponent],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, SidebarComponent, FooterBtnComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/footer-btn/footer-btn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-btn.component.html',
+  styleUrl: './footer-btn.component.css'
+})
+export class FooterBtnComponent {
+
+}
