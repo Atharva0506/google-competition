26142693461861cccd1818fcee4de0f31d9d9995
Atharva Naik <atharvan.coder@gmail.com>
2024-08-03 11:01:18 +0530

    Working on routing (setting page)

--- frontend/src/app/setting/setting.component.ts
+++ frontend/src/app/setting/setting.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-setting',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './setting.component.html',
   styleUrl: './setting.component.css'
 })
