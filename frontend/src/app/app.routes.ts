fd9e38b3f0b61ca9895fd6cc6ed68b29fe233bc8
Atharva Naik <atharvan.coder@gmail.com>
2024-08-06 12:52:01 +0530

    Adding Not Found 404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { SettingComponent } from './pages/setting/setting.component';
 import { HomeComponent } from './pages/home/home.component';
 import { UserPreferenceComponent } from './pages/user-preference/user-preference.component';
 import { AccountComponent } from './pages/account/account.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'setting',component:SettingComponent},
-    {path:'user-preference',component:UserPreferenceComponent},
-    {path:'account',component:AccountComponent}
+    {path:'setting',component:SettingComponent,children:[
+        {path:'account',component:AccountComponent},
+        {path:'user-preference',component:UserPreferenceComponent}
+    ]},
+    {path:'**',component:NotFoundComponent}
 ];
