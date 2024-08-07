f39d404cbe91c3fd03dd7e505aaadca2031e2384
shantanuk7 <shantanukulkarni365@gmail.com>
2024-08-07 19:07:42 +0530

    completed user interests & summary get and post routes and with  gemini AI identifying user info.

--- backend/src/helper/geminiapi/gemini.ts
+++ backend/src/helper/geminiapi/gemini.ts
@@ -1,17 +1,30 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-// import dotenv from "dotenv";
-// dotenv.config();
 
-export default async function gemini(){
-    const genAI = new GoogleGenerativeAI(process.env.GEMINIAPIKEY!);
+interface Interests{
+    interests: string[],
+    country: string
+}
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINIAPIKEY!);
+const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-flash",
+    generationConfig: { responseMimeType: "application/json" }
+});
+
+
+export async function gemini(summaryStyle: string, links: string[]){
     
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    
-    const prompt = "Summarise this news article in the style of how Hagrid from Harry Potter: https://www.rawstory.com/how-a-futuristic-material-is-able-to-change-its-properties-from-soft-to-rigid-and-back/";
-    
+    const prompt = `Given is an array containing links to news-articles. Summarise each article in the style of ${summaryStyle}: ${links}`;
     const result = await model.generateContent(prompt);
-    // console.log(result);
-    console.log(result.response?.text());
 
     return result.response.text();
+}
+
+export async function interestsIdentification(userInput:string): Promise<Interests>{
+
+    const prompt = `Identify interests, categories, locations, and such keywords from the given data, that may be used for searching news articles based on them. If a country is mentioned, store it separately in the object with it's general country code. Respond with an object in the form of this example: {interests: ["keyword1","keyword2"], country: "US"}. IMPORTANT: Respond in valid JSON as per given format. Data: ${userInput}`;
+    const result = await model.generateContent(prompt);
+
+    const data = JSON.parse(result.response.text());
+    return data;
 }
