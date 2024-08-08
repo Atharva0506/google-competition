ff61641bee9dab8c62bd8dcb863cd766c7f18f0b
Atharva Naik <atharvan.coder@gmail.com>
2024-08-08 17:10:50 +0530

    middleware added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,19 +7,21 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { AuthGuard } from './auth.guard';
 
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent},
+    {path:'',component:HomeComponent,canActivate:[AuthGuard]},
     {path:'signup',component:SignupComponent},
     {path:'login',component:LoginComponent},
-    {path:'details',component:DetailsComponent},
+    {path:'details',component:DetailsComponent,canActivate:[AuthGuard]},
     {path:'setting',component:SettingComponent,
         children: [
             { path: 'user-preference', component: UserPreferenceComponent },
             { path: 'account', component: AccountComponent },
             { path: '', redirectTo: 'user-preference', pathMatch: 'full' } 
           ]
+          ,canActivate:[AuthGuard]
     },
     {path:'**',component:NotFoundComponent}
 ];
