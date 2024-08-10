e4950fcd08ade6385199e2bfef93e4e0f26549ec
shantanuk7 <shantanukulkarni365@gmail.com>
2024-08-10 15:21:46 +0530

    Fixed summary format. Now /news route working as expected

--- backend/src/helper/geminiapi/gemini.ts
+++ backend/src/helper/geminiapi/gemini.ts
@@ -6,21 +6,36 @@ interface Interests{
 }
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINIAPIKEY!);
-const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash",
-    generationConfig: { responseMimeType: "application/json" }
-});
 
 
 export async function gemini(summaryStyle: string, links: string[]){
     
-    const prompt = `Given is an array containing links to news-articles. Summarise each article in the style of ${summaryStyle}. URL Links: ${links}. Return a simple string.`;
-    const result = await model.generateContent(prompt);
+    const model = genAI.getGenerativeModel({ 
+        model: "gemini-1.5-flash",
+        systemInstruction:`You are ${summaryStyle}. Your response must a single long string containing summary of all news articles, each separated by two new line characters.`,
+        generationConfig: { 
+            responseMimeType: "text/plain",
+            temperature: 0.8,
+         }
+    });
 
+    const prompt = `Summarise each article's content in the given array of their URL: ${links}.`;
+
+    const result = await model.generateContent(prompt);
+    // const result = "Goldman Sachs CEO David Solomon predicts that the Federal Reserve will not make emergency rate cuts, even as the US economy faces challenges. Solomon believes that the Fed is likely to maintain its current course, even if there is a recession. Meanwhile, fresh renders of the Google Pixel 7 and Pro have been leaked, revealing a refined design with a more prominent camera bump and a unique color option for the Pro model. The Pixel 7 series is expected to be unveiled at Google's annual hardware event in October."
+    console.log(result.response.text());
+    
     return result.response.text();
 }
 
 export async function interestsIdentification(userInput:string): Promise<Interests>{
+
+    const model = genAI.getGenerativeModel({ 
+        model: "gemini-1.5-flash",
+        generationConfig: { 
+            responseMimeType: "application/json",
+         }
+    });
 
     const prompt = `Identify interests, categories, locations, and such keywords from the given data, that may be used for searching news articles based on them. If a country is mentioned, store it separately in the object with it's general country code. Respond with an object in the form of this example: {interests: ["keyword1","keyword2"], country: "US"}. IMPORTANT: Respond in valid JSON as per given format. Data: ${userInput}`;
     const result = await model.generateContent(prompt);
--- backend/src/routes/news.ts
+++ backend/src/routes/news.ts
@@ -25,33 +25,35 @@ interface NewsArticle {
 
 const router: Router = Router();
 
-const data = [
-    {
-        "title": "Goldman’s David Solomon predicts Fed won’t make emergency rate cuts",
-        "description": "Goldman Sachs CEO David Solomon said the Federal Reserve won’t make emergency rate cuts before the central bankers meet in September, despite Monday's Wall Street meltdown.",
-        "content": "Goldman Sachs CEO David Solomon said the Federal Reserve won’t make emergency rate cuts before the central bankers meet in September, despite Monday’s Wall Street meltdown over heightened risks of a recession.\n“I don’t expect that you’ll see anything... [3851 chars]",
-        "url": "https://nypost.com/2024/08/07/business/goldman-sachs-ceo-david-solomon-predicts-federal-reserve-wont-make-emergency-rate-cuts/",
-        "image": "https://nypost.com/wp-content/uploads/sites/2/2024/08/newspress-collage-y3rg3qmkv-1723049769146.jpg?quality=75&strip=all&1723035443&w=1024",
-        "publishedAt": "2024-08-07T16:58:10Z",
-        "source": {
-            "name": "New York Post",
-            "url": "https://nypost.com"
-        }
-    },
-    {
-        "title": "Google's Pixel 7 and 7 Pro’s design gets revealed even more with fresh crisp renders",
-        "description": "Now we have a complete image of what the next Google flagship phones will look like. All that's left now is to welcome them during their October announcement!",
-        "content": "Google’s highly anticipated upcoming Pixel 7 series is just around the corner, scheduled to be announced on October 6, 2022, at 10 am EDT during the Made by Google event. Well, not that there is any lack of images showing the two new Google phones, b... [1419 chars]",
-        "url": "https://www.phonearena.com/news/google-pixel-7-and-pro-design-revealed-even-more-fresh-renders_id142800",
-        "image": "https://m-cdn.phonearena.com/images/article/142800-wide-two_1200/Googles-Pixel-7-and-7-Pros-design-gets-revealed-even-more-with-fresh-crisp-renders.jpg",
-        "publishedAt": "2022-09-28T08:14:24Z",
-        "source": {
-          "name": "PhoneArena",
-          "url": "https://www.phonearena.com"
-        }
-    }
+// const data = [
+//     {
+//         "title": "Goldman’s David Solomon predicts Fed won’t make emergency rate cuts",
+//         "description": "Goldman Sachs CEO David Solomon said the Federal Reserve won’t make emergency rate cuts before the central bankers meet in September, despite Monday's Wall Street meltdown.",
+//         "content": "Goldman Sachs CEO David Solomon said the Federal Reserve won’t make emergency rate cuts before the central bankers meet in September, despite Monday’s Wall Street meltdown over heightened risks of a recession.\n“I don’t expect that you’ll see anything... [3851 chars]",
+//         "url": "https://nypost.com/2024/08/07/business/goldman-sachs-ceo-david-solomon-predicts-federal-reserve-wont-make-emergency-rate-cuts/",
+//         "image": "https://nypost.com/wp-content/uploads/sites/2/2024/08/newspress-collage-y3rg3qmkv-1723049769146.jpg?quality=75&strip=all&1723035443&w=1024",
+//         "publishedAt": "2024-08-07T16:58:10Z",
+//         "source": {
+//             "name": "New York Post",
+//             "url": "https://nypost.com"
+//         }
+//     },
+//     {
+//         "title": "Google's Pixel 7 and 7 Pro’s design gets revealed even more with fresh crisp renders",
+//         "description": "Now we have a complete image of what the next Google flagship phones will look like. All that's left now is to welcome them during their October announcement!",
+//         "content": "Google’s highly anticipated upcoming Pixel 7 series is just around the corner, scheduled to be announced on October 6, 2022, at 10 am EDT during the Made by Google event. Well, not that there is any lack of images showing the two new Google phones, b... [1419 chars]",
+//         "url": "https://www.phonearena.com/news/google-pixel-7-and-pro-design-revealed-even-more-fresh-renders_id142800",
+//         "image": "https://m-cdn.phonearena.com/images/article/142800-wide-two_1200/Googles-Pixel-7-and-7-Pros-design-gets-revealed-even-more-with-fresh-crisp-renders.jpg",
+//         "publishedAt": "2022-09-28T08:14:24Z",
+//         "source": {
+//           "name": "PhoneArena",
+//           "url": "https://www.phonearena.com"
+//         }
+//     }
 
-]
+// ]
+
+let data:NewsArticle[] = [];
 
 const getLinks = (articles: NewsArticle[]):string[] =>{
     const links: string[] =[];
@@ -65,14 +67,12 @@ const getLinks = (articles: NewsArticle[]):string[] =>{
 router.get("/news-articles/", verifyToken, async (req:Request, res:Response)=>{
 
     // Expect the request to be authorised- Bearer token from firebase current user needs to be passed as header from frontend.
-    // Expect uid passed as url param
-    // Expected GET Request URI: keywords[]=category-1&keywords[]=category-2&keywords[]=category-n ...
-    // Expected API Request: https://gnews.io/api/v4/search?q=KEYWORD-1 OR KEYWORD-2 OR ... KEYWORD-N&lang=LANG-CODE&country=CODE&max=5&apikey=GNEWSAPIKEY
+    // API Request: https://gnews.io/api/v4/search?q=KEYWORD-1 OR KEYWORD-2 OR ... KEYWORD-N&lang=LANG-CODE&country=CODE&max=5&apikey=GNEWSAPIKEY
 
-    // const userId: string = req.params.uid;
     const idToken: string = req.headers.authorization!.split(' ')[1];
     const userId: string = await extractUidFromToken(idToken);
     const userInfoData: UserInfo = await getUserInterests(userId);
+
     // const countryCode: string = userInfoData.countryCode.toLowerCase();
 
     console.log(userInfoData);
@@ -80,38 +80,36 @@ router.get("/news-articles/", verifyToken, async (req:Request, res:Response)=>{
     const searchString = userInfoData.interests.join(" OR ");
     console.log(searchString);
 
-    // const response = await axios.get(`https://gnews.io/api/v4/search?q=${searchString}&in=title,description,content&lang=en&&nullable=image&sortby=publishedAt&max=1&apikey=${process.env.GNEWSAPIKEY}`);
-    // const response = await axios.get(`https://gnews.io/api/v4/search?q=election OR olympics&lang=en&country=us&max=1&apikey=${process.env.GNEWSAPIKEY}`);
-    // console.log(`https://gnews.io/api/v4/search?q=${searchString}&lang=en&country=${countryCode}&max=1&apikey=${process.env.GNEWSAPIKEY}`);
-    
-    // console.log(response.data);
+    const response = await axios.get(`https://gnews.io/api/v4/search?q=${searchString}&in=title,description,content&lang=en&&nullable=image&sortby=publishedAt&max=1&apikey=${process.env.GNEWSAPIKEY}`);
 
-    // data = response.data.articles!;
+    console.log(response.data);
+
+    data = response.data.articles!;
+
     res.json(data);
 });
 
 router.get("/news-summary/", verifyToken ,async (req:Request, res:Response)=>{
     //News articles summary using gemini
     // Expect the request to be authorised- Bearer token from firebase current user needs to be passed as header from frontend.
-    // Expected body: { "links": ["https://link1.com/","https://link2.com/"... "https://link3.com/"] }
 
     const idToken: string = req.headers.authorization!.split(' ')[1];
     const userId: string = await extractUidFromToken(idToken);
     const summaryStyle: string = await getUserSummaryStyle(userId);
 
-    if(!data){
+    if(data.length == 0){
         console.log("Data in news articles stored on server: " + data);
         res.json({"Error":"No news articles data found"});
     }
 
-    // const links: string[] = req.body.links;
     const links:string[] = getLinks(data);
     console.log("Sending this for getting summary: \nSummary style: " + summaryStyle + "\nLinks: " + links);
-    
 
-    const response = await gemini(summaryStyle, links);
+    const response:string = await gemini(summaryStyle, links);
 
-    res.json({"summary": response})
+    const finalResponse = {"summary": JSON.stringify(response)};
+
+    res.json(finalResponse)
 });
 
 export default router;
