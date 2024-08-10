7fd5446032f557500a8a9986ca5057f5179f8475
Atharva Naik <atharvan.coder@gmail.com>
2024-08-10 08:19:29 +0530

    Done with news feed

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsService } from '../../service/news/news.service';
 
 @Component({
   selector: 'app-main',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  summary: string = '';
 
+  constructor(private newsService: NewsService) { }
+
+  ngOnInit(): void {
+    this.newsService.getNewsSummary().subscribe(data => {
+      this.summary = data;
+      console.log('Summary:', JSON.stringify(this.summary, null, 2));
+    }, error => {
+      console.error('Error fetching news summary:', error);
+    });
+  }
+  
 }
--- frontend/src/app/service/news/news.service.ts
+++ frontend/src/app/service/news/news.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-  private apiUrl = 'http://localhost:3000/api/news/news-articles'; 
+  private apiUrl = 'http://localhost:3000/api';
+  private links: string[] = [];
+  private data: any[] = [];
 
   constructor(private http: HttpClient) { }
 
-  getNewsArticles(token: string): Observable<any> {
+  // Get News Articles
+  getNewsArticles(token: string): Observable<any[]> {
     const headers = new HttpHeaders().set('Authorization', `${token}`);
-    return this.http.get<any>(`${this.apiUrl}`,{ headers});
+
+    return this.http.get<any[]>(`${this.apiUrl}/news/news-articles`, { headers }).pipe(
+      tap(data => {
+        // Store the data and extract links
+        this.data = data;
+        this.links = data.map(article => article.url).filter(url => url !== undefined);
+      })
+    );
+  }
+
+  // Get Summary
+  getNewsSummary(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      links: this.links,
+      'Authorization': token ? `${token}` : ''
+    });
+
+    return this.http.get<any>(`${this.apiUrl}/news/news-summary/`, { headers });
   }
 }
