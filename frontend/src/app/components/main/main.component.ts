76a958e78c3e28cc9a95455b6167490f8f663e1f
Atharva Naik <atharvan.coder@gmail.com>
2024-08-12 10:26:48 +0530

    scroll bar

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -12,11 +12,8 @@ import { MarkdownComponent } from 'ngx-markdown';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-  summary: string = `## Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa nihil, facere quae fugiat labore quis, modi doloribus blanditiis qui, eveniet placeat rerum quidem aliquid. Animi quidem explicabo iusto id harum. Nihil quam excepturi impedit magnam harum omnis maxime perspiciatis culpa itaque eaque eligendi nobis hic rerum, optio unde ex.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, deleniti. Sit tempore ipsa quis omnis fugit, quibusdam consequuntur et quidem provident, beatae, labore fugiat! Nostrum vero, nihil nulla consequatur enim quidem laborum exercitationem ## dolorum aliquam hic maxime, magni sapiente quia ipsam doloremque, sequi ex earum molestiae. Quae saepe aperiam nemo, omnis aut cumque totam amet quod assumenda id tempore perspiciatis culpa impedit quam similique nisi accusamus veritatis ad nostrum ratione? Ratione commodi mollitia consequuntur rerum ad excepturi eveniet nemo fugit iste, ex sed officia et voluptate aliquam neque quae quisquam. Iure nostrum sit illo perspiciatis debitis voluptatum, assumenda laborum, quibusdam incidunt error aut quae, ab numquam minus deleniti ducimus recusandae esse id amet nihil nulla ##laudantium. Inventore provident, nihil explicabo quia error natus officia laboriosam ea aut corporis, quidem magnam maxime assumenda? Blanditiis provident quis soluta cupiditate illum modi optio at hic, possimus fugit, iure aut, quia neque. Dolor minima quis architecto non alias. Aperiam nesciunt porro est, neque error ratione perferendis, assumenda optio culpa veniam fugiat quo ab illum, earum corrupti illo quibusdam beatae ipsam amet totam nostrum! Sed, autem, alias deserunt voluptates enim qui omnis nostrum reiciendis delectus odio impedit, debitis laboriosam aliquam veniam. Iure odio quidem incidunt?
-## lor minima quis architecto non alias. Aperiam nesciunt porro est, neque error ratione perferendis, assumenda optio culpa veniam fugiat quo ab illum, earum corrupti illo quibusdam beatae ipsam amet totam nostrum! Sed, autem, alias deserunt voluptates enim qui omnis nostrum reiciendis delectus odio impedit, debitis laboriosam aliquam veniam. Iure odio quidem incidunt?
-';`
-  loading: boolean = false;
+  summary: string = '';
+  loading: boolean = true;
   constructor(private newsService: NewsService) {}
 
   ngOnInit(): void {
