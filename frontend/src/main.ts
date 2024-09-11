bbc6fc821b582519f5b57154125a4d94d7ea86eb
Atharva Naik <atharvan.coder@gmail.com>
2024-09-11 08:40:04 +0530

    making ux better

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,3 +4,17 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+(function () {
+  const originalAddEventListener = EventTarget.prototype.addEventListener;
+  EventTarget.prototype.addEventListener = function (type, listener, options) {
+    // Default to passive: true for scroll-blocking events
+    const shouldModifyOptions =
+      (type === 'scroll' || type === 'wheel' || type === 'touchstart' || type === 'touchmove') &&
+      !options;
+    const newOptions = shouldModifyOptions
+      ? { passive: true, capture: false }
+      : options;
+    return originalAddEventListener.call(this, type, listener, newOptions);
+  };
+})();
